b75fb141facf019737d5aecd0defa749dd4b2848
Saad5400 <sdbtwa@gmail.com>
2023-06-17 22:28:37 +0300

    solved 6

---
+++ 6. Zigzag Conversion/Program.cs
@@ -0,0 +1,44 @@
+﻿string Convert(string s, int numRows)
+{
+        if (numRows == 1) return s;
+
+        var arr = new List<string[]>();
+
+        int i = 0, j = 0, k = 0;
+        while (i < s.Length)
+        {
+            if (arr.Count <= j) arr.Add(new string[numRows]);
+            if (k == 0)
+            {
+                for (int z = 0; z < numRows && i < s.Length; z++)
+                {
+                    arr[j][z] = s[i].ToString();
+                    i++;
+                }
+                k = numRows - 2;
+                j++;
+            }
+            else while (k != 0 && i < s.Length)
+            {
+                if (arr.Count <= j) arr.Add(new string[numRows]);
+                arr[j][k] = s[i].ToString();
+                j++;
+                i++;
+                k--;
+            }
+        }
+        
+        string result = string.Empty;
+
+        for (int x = 0; x < numRows; x++)
+        {
+            for (int y = 0; y < arr.Count; y++)
+            {
+                result += arr[y][x] is null ? string.Empty : arr[y][x];
+            }
+        }
+        
+        return result;
+}
+
+Console.WriteLine(Convert("ABCDE", 4));
