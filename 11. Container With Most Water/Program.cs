827bda7ae71c547664a884ba287ceba1e4ae8204
Saad5400 <sdbtwa@gmail.com>
2023-06-23 00:22:28 +0300

    solved 11

---
+++ 11. Container With Most Water/Program.cs
@@ -0,0 +1,18 @@
+﻿int MaxArea(int[] height)
+{
+    var left = 0;
+    var right = height.Length - 1;
+    var answer = 0;
+
+    while (left < right)
+    {
+        answer = Math.Max(answer, (right - left) * Math.Min(height[left], height[right]));
+        if (height[left] > height[right])
+            --right;
+        else
+            ++left;
+    }
+
+    return answer;
+}
+Console.WriteLine(MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7, 1 }));
