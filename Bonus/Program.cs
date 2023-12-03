9e11069d1a37f06dde76fa04524fb47f7343ac74
Saad5400 <sdbtwa@gmail.com>
2023-12-03 11:11:33 +0300

    attempt

---
+++ Bonus/Program.cs
@@ -0,0 +1,52 @@
+﻿int PossibleBracesCombinations(int duoCount)
+{
+    var result = 0;
+    var count = duoCount * 2;
+    var possibilities = Math.Pow(2, duoCount);
+    var list = new bool[duoCount];
+    for (int i = 0; i < list.Length; i++)
+    {
+        list[i] = true;
+    }
+    var startIndex = 0;
+    var currentIndex = 1;
+
+    for (int i = 0; i < possibilities; i++)
+    {
+        var combination = string.Empty;
+        for (int j = 0; j < duoCount; j++)
+        {
+            if (list[j])
+            {
+                combination += "(";
+            }
+            else
+            {
+                combination += ")";
+            }
+            if (currentIndex > startIndex && currentIndex == j)
+            {
+                list[j] = !list[j];
+                currentIndex++;
+            }
+        }
+        currentIndex = startIndex++;
+        var temp = combination;
+        for (int j = duoCount - 1; j >= 0; j--)
+        {
+            if (temp[j] == '(')
+            {
+                combination += ")";
+            }
+            else
+            {
+                combination += "(";
+            }
+        }
+        Console.WriteLine(combination);
+    }
+
+    return result;
+}
+
+Console.WriteLine(PossibleBracesCombinations(3));
